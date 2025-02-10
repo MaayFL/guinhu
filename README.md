9354e955c01c50c3a30366f887d094d88710a1d5
MaayFL <kindergardennugget6@gmail.com>
2025-02-10 14:39:45 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# guinhu
-gui Nhu
+#Gui Nhu
---
+++ script.js
@@ -0,0 +1,14 @@
+function moveRandomEL(elm) {
+    elm.style.position = "absolute";
+    elm.style.top = Math.floor(Math.random() * 90 + 5)
+    +"%";
+    elm.style.left = Math.floor(Math.random() * 90 + 
+    5)+"%";
+}
+
+const moverandom = document.querySelector
+("#move-random");
+moverandom.addEventListener("mouseenter", function
+(e) {
+    moveRandomEL(e.target);
+});
